b9206b331562ebab36ebaf30efd4a82f72e8279c
moloxiao <molo.xiao@gmail.com>
2013-10-21 16:34:13 +0800

    添加倒计时Sprite
    添加倒计时Sprite

--- src/com/molocode/sudoku/game/sprite/CountdownSprite.java
+++ src/com/molocode/sudoku/game/sprite/CountdownSprite.java
@@ -13,15 +13,22 @@ public class CountdownSprite extends Sprite {
 	public static final int WIDTH = 280;
 	public static final int HEIGHT = 80;
 	
+	private float elapse;
+	
 	public CountdownSprite(IBitmapRes bitmapRes, int pX, int pY, int width,
 			int height) {
 		super(bitmapRes, pX, pY, width, height);
 	}
 	
+	public void cleanCountTime() {
+		elapse = 0.0f;
+	}
+	
 	@Override
 	public void onDrawSelf(Canvas canvas) {
-		float width = PaintManager.getInstance().getTextBlackPaint().measureText("00:00");
-		canvas.drawText("00:00", 
+		String time = CountdownSprite.getTime(elapse);
+		float width = PaintManager.getInstance().getTextBlackPaint().measureText(time);
+		canvas.drawText(time, 
 				getX() + (getWidth()-width)/2, 
 				getY() + 60*EngineOptions.getScreenScaleY(), 
 				PaintManager.getInstance().getTextBlackPaint());
@@ -29,8 +36,33 @@ public class CountdownSprite extends Sprite {
 	
 	@Override
 	protected void onUpdateSelf(float secondsElapsed) {
-		// TODO : 计时器
-		// TODO : 计时器到后要弹出失败的界面
+		elapse += secondsElapsed;
+	}
+	
+	public static String getTime(float totalSecondsElapsed) {
+		int time = (int)totalSecondsElapsed;
+		if(time < 60 ) {
+			if(time < 10) {
+				return "00:0" + time;
+			}else {
+				return "00:" + time;
+			}
+		}else {
+			int fen = time/60;
+			int second = time%60;
+			StringBuilder sb = new StringBuilder();
+			if(fen >= 10) {
+				sb.append(fen+":");
+			}else {
+				sb.append("0" + fen + ":");
+			}
+			if(second >= 10){
+				sb.append(second+"");
+			}else {
+			sb.append("0" + second);
+			}
+			return sb.toString();
+		}
 	}
 	
 }

7b0d6532301fa4fb6b63b450a046ef8081969620
qz515880 <1195933889@qq.com>
2014-07-11 00:13:49 +0800

    考试
    Examination

---
+++ src/com/molocode/sudoku/yard/activity/CourseTreeActivity.java
@@ -0,0 +1,23 @@
+package com.molocode.sudoku.yard.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CourseTreeActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// TODO 设置课程树
+		// setContentView(layoutResID);
+	}
+
+	// TODO 首次登陆的用户弹考试界面
+	private void showExamination() {
+
+	}
+
+	// TODO 点击参加考试按钮，进入游戏界面
+	private void enterExamination() {
+
+	}
+}
---
+++ src/com/molocode/sudoku/yard/activity/SchoolTreeActivity.java
@@ -0,0 +1,20 @@
+package com.molocode.sudoku.yard.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SchoolTreeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// TODO 设置学校路线
+		// setContentView(layoutResID);
+	}
+
+	// TODO 首次登陆的用户弹出欢迎入学界面
+	private void showAdmission() {
+
+	}
+	
+}

feb6ca496f425869fed14ca33f232a5d6230d018
qz515880 <1195933889@qq.com>
2014-07-15 20:05:14 +0800

    DegreeManger实现

--- src/com/molocode/sudoku/Journey/degree/DegreeManager.java
+++ src/com/molocode/sudoku/Journey/degree/DegreeManager.java
@@ -1,10 +1,24 @@
 package com.molocode.sudoku.Journey.degree;
 
+import android.util.Log;
+
 public class DegreeManager {
 
-	
 	public static Degree getDegree(int degreeId) {
-		// TODO : 等待实现，需要依次是小学中学。。。
-		return null;
+		switch (degreeId) {
+		case Degree.PRIMARY:
+			return new PrimaryDegree();
+		case Degree.MIDDLE:
+			return new MiddleDegree();
+		case Degree.HIGH:
+			return new HighDegree();
+		case Degree.UNIVERSITY:
+			return new UniversityDegree();
+		case Degree.POSTGRADUATE:
+			return new PostgraduateDegree();
+		default:
+			Log.e("com.poxiao.suduko", "无此学历,去火星看看");
+			return null;
+		}
 	}
 }

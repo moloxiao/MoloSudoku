9ce8477abc4f907da0b2649eda0c476ccd496af9
qz515880 <1195933889@qq.com>
2014-07-15 19:47:15 +0800

    Revert "111"
    This reverts commit 8f87baf1936503e00aa6ad9523586cad02b9da04.

--- src/com/molocode/sudoku/game/domain/LifeJourney.java
+++ src/com/molocode/sudoku/game/domain/LifeJourney.java
@@ -1,20 +1,17 @@
 package com.molocode.sudoku.game.domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
 public class LifeJourney {
 
 	private static LifeJourney journery = null;
-	private int currentDegreeId;
-
-	public int getCurrentDegreeId() {
-		return currentDegreeId;
-	}
-
-	public void setCurrentDegreeId(int currentDegreeId) {
-		this.currentDegreeId = currentDegreeId;
-	}
+	private List<Degree> degrees;
 
 	private LifeJourney() {
-		//
+		getDegreeByMap(getDefaultMap());
 	}
 
 	public static LifeJourney getInstance() {
@@ -25,4 +22,35 @@ public class LifeJourney {
 		}
 	}
 
+	public List<Degree> getDegrees() {
+		return degrees;
+	}
+
+	public void setDegrees(List<Degree> degrees) {
+		this.degrees = degrees;
+	}
+
+	// 设置地图上所有学历的信息
+	public void getDegreeByMap(Map<Integer, String> map) {
+		degrees = new ArrayList<Degree>();
+		for (int i = 0; i < map.size(); i++) {
+			String degreeinfo = map.get(i);
+			String[] infos = degreeinfo.split("-");
+			Degree degree = new Degree(Integer.valueOf(infos[0]));
+			degree.setDegreeName(infos[1]);
+			degrees.add(degree);
+		}
+	}
+
+	// 获取默认的关卡配置
+	private Map<Integer, String> getDefaultMap() {
+		// 构造的时候可以从文件里读出degreesMap:degreeId-degreeName-degreeType
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		map.put(0, "0-小学");
+		map.put(1, "1-中学");
+		map.put(2, "2-高中");
+		map.put(3, "3-大学");
+		map.put(4, "4-硕士");
+		return map;
+	}
 }
